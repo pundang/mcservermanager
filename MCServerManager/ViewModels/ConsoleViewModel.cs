920e81085c42ee3ccc3f8f5369e8d5e3ad9f929f
pundang <40328573+pundang@users.noreply.github.com>
2026-07-15 17:26:53 -0300

    feat: implement ConsoleView for console output and command input

--- MCServerManager/ViewModels/ConsoleViewModel.cs
+++ MCServerManager/ViewModels/ConsoleViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
 using MCServerManager.Services;
 
 namespace MCServerManager.ViewModels;
@@ -13,5 +14,14 @@ public partial class ConsoleViewModel : ViewModelBase
     {
         _processService = processService;
         _processService.OutputReceived += (_, line) => ConsoleOutputLines.Add(line);
+    }
+
+    /// <summary>
+    /// Send command to the running server
+    /// </summary>
+    [RelayCommand]
+    private void SendCommand(string command)
+    {
+        _processService.SendCommandAsync(command);
     }
 }
--- MCServerManager/Views/ConsoleView.axaml.cs
+++ MCServerManager/Views/ConsoleView.axaml.cs
@@ -1,11 +1,59 @@
 using Avalonia.Controls;
+using MCServerManager.ViewModels;
 
 namespace MCServerManager.Views;
 
 public partial class ConsoleView : UserControl
 {
+    private bool _isAtBottom = true;
+
     public ConsoleView()
     {
         InitializeComponent();
+    }
+
+    private void SendCommand()
+    {
+        var command = CmdBox.Text;
+        if (command == string.Empty)
+            return;
+
+        if (DataContext is ConsoleViewModel consoleViewModel)
+        {
+            consoleViewModel.SendCommandCommand.Execute(command);
+        }
+
+        CmdBox.Text = string.Empty;
+    }
+
+    private void LogScrollList_ScrollChanged(object? sender, ScrollChangedEventArgs e)
+    {
+        if (sender is not ScrollViewer scrollViewer)
+            return;
+
+        // Detect if the user manually scrolled to the bottom
+        double maxOffset = scrollViewer.Extent.Height - scrollViewer.Viewport.Height;
+
+        if (e.ExtentDelta.Length == 0)
+        {
+            // The user is actively scrolling manually
+            _isAtBottom = (maxOffset - scrollViewer.Offset.Y) <= 1.0;
+        }
+        else if (_isAtBottom && e.ExtentDelta.Y > 0)
+        {
+            // Content was added, and the user was already at the bottom
+            scrollViewer.ScrollToEnd();
+        }
+    }
+
+    private void CmdBox_KeyDown(object? sender, Avalonia.Input.KeyEventArgs e)
+    {
+        if (e.Key == Avalonia.Input.Key.Enter)
+            SendCommand();
+    }
+
+    private void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        SendCommand();
     }
 }
